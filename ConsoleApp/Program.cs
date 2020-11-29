259e5418051e8970b213c3559947478a251e82a3
Jim Yarbro <nrrdio@outlook.com>
2020-11-29 14:02:54 +0100

    Basic hello-world test against the endpoint

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,43 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using YnabFeeder.Common;
 
-Console.WriteLine("Hello World!");
+Console.CancelKeyPress += ConsoleCancelKeyPress;
+
+var services = ConfigureServices();
+
+var test = services.BuildServiceProvider().GetService<YnabFeeder.Class1>();
+
+await test.Test();
+
+
+static ServiceCollection ConfigureServices() {
+    var configurationBuilder = new ConfigurationBuilder();
+
+    var configuration = configurationBuilder
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+    var services = new ServiceCollection();
+
+    services.Configure<FintsOptions>((options) => {
+        configuration.GetSection("fints").Bind(options);
+    });
+
+    services.Configure<YnabOptions>((options) => {
+        configuration.GetSection("ynab").Bind(options);
+    });
+
+    services.AddScoped<IConfiguration>((services) => configuration);
+
+    services.AddScoped<YnabFeeder.Class1>();
+
+    return services;
+}
+
+static void ConsoleCancelKeyPress(object sender, ConsoleCancelEventArgs e) {
+    e.Cancel = true;
+
+    // ... cleanup and shutdown ...
+}
--- YnabFeeder/Class1.cs
+++ YnabFeeder/Class1.cs
@@ -1,6 +1,49 @@
-﻿using System;
+﻿using libfintx;
+using libfintx.Data;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+using YnabFeeder.Common;
 
 namespace YnabFeeder {
     public class Class1 {
+        readonly FintsOptions Options;
+
+        public Class1(
+            IOptions<FintsOptions> options
+        ) {
+            Options = options.Value;
+        }
+
+        public async Task Test() {
+            foreach (var bank in Options.Banks) {
+                var connection = new ConnectionDetails();
+                connection.HbciVersion = 300;
+                connection.Blz = bank.Blz;
+                connection.UserId = bank.UserId;
+                connection.Pin = bank.Pin;
+                connection.Url = bank.Endpoint;
+
+                var client = new FinTsClient(connection);
+
+                var tanDialog = new TANDialog((dialog) => {
+                    foreach (var message in dialog.DialogResult.Messages) {
+                        Console.WriteLine($"{message.Code} {message.Message}");
+                    }
+
+                    return Task.Run(() => string.Empty);
+                });
+
+                var result = await client.Accounts(tanDialog);
+
+                foreach (var message in result.Messages) {
+                    Console.WriteLine($"{message.Code} {message.Message}");
+                }
+
+                foreach (var account in result.Data) {
+                    Console.Write($"IBAN: {account.AccountIban}");
+                }
+            }
+        }
     }
 }
