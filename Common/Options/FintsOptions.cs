30420286a042becd59246e86b68017c6ecf5159d
Jim Yarbro <nrrdio@outlook.com>
2020-12-01 10:54:45 +0100

    moved

--- Common/Options/FintsOptions.cs
+++ Common/Options/FintsOptions.cs
@@ -3,6 +3,7 @@ using YnabFeeder.Common.Models;
 
 namespace YnabFeeder.Common {
     public class FintsOptions {
+        public string FilePath { get; set; }
         public List<Bank> Banks { get; set; }
     }
 }
