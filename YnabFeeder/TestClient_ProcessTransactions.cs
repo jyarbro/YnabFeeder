95d70d457c57603ec175ba880f4dfb3341616a66
Jim Yarbro <nrrdio@outlook.com>
2020-12-05 10:36:49 +0100

    test several transactions against ynab

--- YnabFeeder/TestClient_ProcessTransactions.cs
+++ YnabFeeder/TestClient_ProcessTransactions.cs
@@ -1,5 +1,6 @@
 ﻿using libfintx.Swift;
 using Microsoft.Extensions.Options;
+using Nrrdio.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,6 @@ using YNAB.SDK.Api;
 using YNAB.SDK.Client;
 using YNAB.SDK.Model;
 using YnabFeeder.Common;
-using YnabFeeder.Common.Utilities;
 
 namespace YnabFeeder {
     public class TestClient_ProcessTransactions {
@@ -29,7 +29,7 @@ namespace YnabFeeder {
 
         public async Task Run() {
             OpenConnection();
-            await TestTransaction();
+            await ProcessTransactions();
         }
 
         void OpenConnection() {
@@ -37,22 +37,41 @@ namespace YnabFeeder {
             Budget = Client.Budgets.GetBudgetById(YnabOptions.BudgetId).Data.Budget;
         }
 
-        async Task TestTransaction() {
-            var payee = Budget.Payees[4];
+        async Task ProcessTransactions() {
+            var swiftStatements = JsonFiles.ReadFromJsonFile<List<SwiftStatement>>($"{FintsOptions.FilePath}\\transactions.json");
 
-            var transaction = new SaveTransaction {
-                AccountId = Budget.Accounts.First().Id,
-                Amount = 100L,
-                Date = DateTime.Now,
-                PayeeId = payee.Id,
-                PayeeName = payee.Name,
-                CategoryId = Budget.Categories.First().Id,
-                Memo = "Test Transaction",
-                Approved = true,
-                Cleared = SaveTransaction.ClearedEnum.Cleared
-            };
+            var accountId = new Guid(FintsOptions.Banks.First().Accounts.First().YnabAccountId);
+            var account = Budget.Accounts.First(o => o.Id == accountId);
 
-            await Client.Transactions.CreateTransactionAsync(YnabOptions.BudgetId, new SaveTransactionsWrapper(transaction));
+            var ynabTransactions = new List<SaveTransaction>();
+
+            var importIds = new List<string>();
+
+            foreach (var swiftStatement in swiftStatements) {
+                foreach (var swiftTransaction in swiftStatement.SwiftTransactions) {
+                    var milliunits = Convert.ToInt64(swiftTransaction.Amount * 1000);
+                    var importIdBase = $"NRRDIO:{milliunits}:{swiftTransaction.ValueDate:d}";
+
+                    importIds.Add(importIdBase);
+                    var occurrences = importIds.Count(o => o == importIdBase);
+
+                    var importId = $"{importIdBase}:{occurrences}";
+
+                    ynabTransactions.Add(new SaveTransaction {
+                        AccountId = account.Id,
+                        Amount = milliunits,
+                        Date = swiftTransaction.ValueDate,
+                        PayeeName = swiftTransaction.PartnerName,
+                        Memo = swiftTransaction.SVWZ,
+                        Cleared = SaveTransaction.ClearedEnum.Cleared,
+                        ImportId = importId
+                    });
+                }
+            }
+
+            var response = await Client.Transactions.CreateTransactionAsync(YnabOptions.BudgetId, new SaveTransactionsWrapper(transactions: ynabTransactions));
+
+            Console.WriteLine($"{response.Data.DuplicateImportIds.Count} duplicates found. {response.Data.TransactionIds.Count} added.");
         }
     }
 }
