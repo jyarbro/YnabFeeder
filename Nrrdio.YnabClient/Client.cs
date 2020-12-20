2f50411bd239b3516015703f8756d6af1d1f697e
Jim Yarbro <nrrdio@outlook.com>
2020-12-20 10:58:07 +0100

    Moved hostbuilder to root.

--- Nrrdio.YnabClient/Client.cs
+++ Nrrdio.YnabClient/Client.cs
@@ -1,15 +1,9 @@
 ﻿using libfintx;
 using libfintx.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Nrrdio.Utilities.Loggers;
-using Nrrdio.Utilities.Loggers.Contracts;
 using Nrrdio.YnabClient.Options;
-using Nrrdio.YnabClient.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -148,39 +142,5 @@ namespace Nrrdio.YnabClient {
         }
 
         public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host
-                .CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) => {
-                    services.AddHostedService<Client>();
-                    
-                    var configurationBuilder = new ConfigurationBuilder();
-
-                    var configuration = configurationBuilder
-                        .AddJsonFile("appsettings.json")
-                        .Build();
-
-                    services.Configure<FintsOptions>((options) => {
-                        configuration.GetSection("fints").Bind(options);
-                    });
-
-                    services.Configure<YnabOptions>((options) => {
-                        configuration.GetSection("ynab").Bind(options);
-                    });
-
-                    services.AddScoped<IConfiguration>((services) => configuration);
-
-                    services.AddScoped((services) => {
-                        var ynabOptions = services.GetService<IOptions<YnabOptions>>().Value;
-                        return new API(ynabOptions.AccessToken);
-                    });
-
-                    services.AddDbContext<DataContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
-
-                    services.AddSingleton<ILogEntryRepository, LogEntryRepository>();
-                    services.AddSingleton<ILoggerProvider, DatabaseLoggerProvider>();
-                    services.AddLogging();
-                });
     }
 }
--- Nrrdio.YnabClient/Program.cs
+++ Nrrdio.YnabClient/Program.cs
@@ -1,4 +1,47 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Nrrdio.Utilities.Loggers;
+using Nrrdio.Utilities.Loggers.Contracts;
 using Nrrdio.YnabClient;
+using Nrrdio.YnabClient.Options;
+using Nrrdio.YnabClient.Services;
+using YNAB.SDK;
 
-await Client.CreateHostBuilder(args).Build().RunAsync();
+await Host
+    .CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) => {
+        services.AddHostedService<Client>();
+
+        var configurationBuilder = new ConfigurationBuilder();
+
+        var configuration = configurationBuilder
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        services.Configure<FintsOptions>((options) => {
+            configuration.GetSection("fints").Bind(options);
+        });
+
+        services.Configure<YnabOptions>((options) => {
+            configuration.GetSection("ynab").Bind(options);
+        });
+
+        services.AddScoped<IConfiguration>((services) => configuration);
+
+        services.AddScoped((services) => {
+            var ynabOptions = services.GetService<IOptions<YnabOptions>>().Value;
+            return new API(ynabOptions.AccessToken);
+        });
+
+        services.AddDbContext<DataContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddSingleton<ILogEntryRepository, LogEntryRepository>();
+        services.AddSingleton<ILoggerProvider, DatabaseLoggerProvider>();
+        services.AddLogging();
+    })
+    .Build()
+    .RunAsync();
